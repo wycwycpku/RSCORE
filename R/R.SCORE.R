ae6999d24b48cc5cd8d4c76408dd40bd68215a7b
wycpku <wuyichongyt@sina.cn>
2019-06-07 12:45:34 +0800

    add vigette

--- R/R.SCORE.R
+++ R/R.SCORE.R
@@ -22,6 +22,9 @@ R.SCORE <- function(Data, PPI = 'Biogrid', species = 9606, score_threshold = 600
                   metric = c('cor','rho','phi','phs'),
                   module_min = 3, max_step = 5, AUCRank = 400)
 {
+  library(Seurat)
+  library(AUCell)
+  library(propr)
   ## data processing
   if(class(Data) == 'matrix'){
     Data <- mat2seurat(Data)
